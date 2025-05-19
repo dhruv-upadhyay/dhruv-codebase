3088ae97bef45d1e5a03aba983d5c0d9016b696b
Dhruv Upadhyay <dhruv.upadhyay8458@gmail.com>
2025-05-19 13:50:52 +0530

    remove unwanted folder structure and also remove git ignore hidden folder
    remove unwanted folder structure and also remove git ignore hidden folder

---
+++ App_Challenges(Swift)/BlueApps/BlueApp-SwiftUI/BlueApp/App/SanskritiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueApp.swift
+//  BlueApp
+//
+//  Created by Dhruv Upadhyay on 19/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BlueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
